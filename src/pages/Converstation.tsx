b707c41fa1cd93e79f7f0f0c00ddff3f9d811bf6
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2023-11-09 20:51:36 +0300

    Added conservations in  local storage

--- src/pages/Converstation.tsx
+++ src/pages/Converstation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { Layout, Menu } from 'antd';
 import { BiMessageAlt } from 'react-icons/bi';
 import { FaPlus } from 'react-icons/fa';
@@ -6,15 +6,17 @@ import axios from 'axios';
 
 const { Header, Content, Sider } = Layout;
 
-// TODO: Lenght => User'a ait coseverstation sayısı olacak.
-
-
 const Converstation: React.FC = () => {
-
     const token = localStorage.getItem('token');
 
-    const [conversations, setConversations] = useState<Array<{ key: React.Key; icon: React.ReactNode; label: React.ReactNode; className?: string }>>([]);
+    // Converstation dizisi
+    const [conversations, setConversations] = useState<Array<any>>(() => {
+        const storedConversations = JSON.parse(localStorage.getItem('conversations') || '[]');
+        return storedConversations;
+    });
 
+
+    const [convserstationName, setConvserstationName] = useState<string>('');
 
     const createConverstation = async () => {
         try {
@@ -26,53 +28,65 @@ const Converstation: React.FC = () => {
                 },
             });
 
+            setConvserstationName(response.data.name);
+            console.log(convserstationName)
+
+            console.log(response.data)
+
 
             const newConversation = {
-                key: response.data.id,
+                key: String(conversations.length + 1),
                 icon: <BiMessageAlt />,
-                label: response.data.name,
+                label: ` Converstation ${conversations.length + 1} `,
                 className: 'font-inter text-regular',
             };
 
-            setConversations((prevConversations) => [...prevConversations, newConversation]);
+            setConversations(prevConversations => [...prevConversations, newConversation]);
 
-            console.log(response.data);
+            // localStorage güncelleniyor
+            localStorage.setItem('conversations', JSON.stringify([...conversations, newConversation]));
+
         } catch (error) {
             console.error(error);
         }
-    }
+    };
+
+    const items: React.ComponentProps<typeof Menu>['items'] = Array.from({ length: conversations.length }, (_, index) => ({
+        key: String(index + 1),
+        icon: <BiMessageAlt />,
+        label: "Converstation",
+        className: 'font-inter text-regular',
+    }));
+
+    useEffect(() => {
+
+        // Sayfa yüklendiğinde localStorage'dan verileri al
+        const storedConversations = JSON.parse(localStorage.getItem('conversations') || '[]');
+        setConversations(storedConversations);
+    }, []);
+
 
     return (
-
         <Layout className="flex">
-
-            {/* Soldaki Menü */}
             <Sider className="overflow-auto h-screen fixed left-0 top-0 bottom-0 " style={{ background: '#101010' }}>
-
-                {/* New Chat Butonu */}
                 <button
-                    onClick={createConverstation} className="bg-[#202123] hover:opacity-80 text-white font-inter font-medium py-2 px-4 mt-4 ml-2 rounded-md flex items-center">
-                    <FaPlus className="mr-2" /> New
+                    onClick={createConverstation}
+                    className="bg-[#202123] hover:opacity-80 text-white font-inter font-medium py-2 px-4 mt-4 ml-2 rounded-md flex items-center"
+                >
+                    <FaPlus className="mr-2" /> New Chat
                 </button>
 
-                {/* Sohbet Menüsü */}
                 <div className="mt-6" style={{ background: '#101010' }} />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={conversations} style={{ background: '#101010' }} />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={items} style={{ background: '#101010' }} />
             </Sider>
 
-            {/* İçerik */}
-
             <Layout className="site-layout ml-64">
-
                 <Header className="p-0" style={{ background: 'white' }} />
-
-
                 <Content className="m-24 overflow-visible">
                     <div className={`p-24 text-center`}>
                         <p>Konuşma İçeriği</p>
                     </div>
                 </Content>
-
             </Layout>
         </Layout>
     );
