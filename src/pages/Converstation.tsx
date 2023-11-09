f68a44763651fc4e26dc6fb074e2fde5193b520b
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2023-11-09 20:02:26 +0300

    Fixed converstations

--- src/pages/Converstation.tsx
+++ src/pages/Converstation.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-import type { MenuProps } from 'antd';
+import React, { useState } from 'react'
 import { Layout, Menu } from 'antd';
 import { BiMessageAlt } from 'react-icons/bi';
 import { FaPlus } from 'react-icons/fa';
@@ -9,15 +7,14 @@ import axios from 'axios';
 const { Header, Content, Sider } = Layout;
 
 // TODO: Lenght => User'a ait coseverstation sayısı olacak.
-const items: MenuProps['items'] = Array.from({ length: 9 }, (_, index) => ({
-    key: String(index + 1),
-    icon: <BiMessageAlt />,
-    label: `Conversation ${index + 1}`,
-    className: 'font-inter text-regular',
-}));
+
 
 const Converstation: React.FC = () => {
-    const token = localStorage.getItem('token')
+
+    const token = localStorage.getItem('token');
+
+    const [conversations, setConversations] = useState<Array<{ key: React.Key; icon: React.ReactNode; label: React.ReactNode; className?: string }>>([]);
+
 
     const createConverstation = async () => {
         try {
@@ -29,46 +26,54 @@ const Converstation: React.FC = () => {
                 },
             });
 
+
+            const newConversation = {
+                key: response.data.id,
+                icon: <BiMessageAlt />,
+                label: response.data.name,
+                className: 'font-inter text-regular',
+            };
+
+            setConversations((prevConversations) => [...prevConversations, newConversation]);
+
             console.log(response.data);
         } catch (error) {
             console.error(error);
         }
     }
 
-
-
     return (
+
         <Layout className="flex">
 
-            {/* Left Side */}
+            {/* Soldaki Menü */}
             <Sider className="overflow-auto h-screen fixed left-0 top-0 bottom-0 " style={{ background: '#101010' }}>
 
-                {/* New Chat Button */}
+                {/* New Chat Butonu */}
                 <button
                     onClick={createConverstation} className="bg-[#202123] hover:opacity-80 text-white font-inter font-medium py-2 px-4 mt-4 ml-2 rounded-md flex items-center">
-                    <FaPlus className="mr-2" /> New Chat
+                    <FaPlus className="mr-2" /> New
                 </button>
 
-                {/* Converstation */}
+                {/* Sohbet Menüsü */}
                 <div className="mt-6" style={{ background: '#101010' }} />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={items} style={{ background: '#101010' }} />
-
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} items={conversations} style={{ background: '#101010' }} />
             </Sider>
 
-            {/* Content */}
+            {/* İçerik */}
+
             <Layout className="site-layout ml-64">
 
                 <Header className="p-0" style={{ background: 'white' }} />
+
 
                 <Content className="m-24 overflow-visible">
                     <div className={`p-24 text-center`}>
                         <p>Konuşma İçeriği</p>
                     </div>
-
                 </Content>
 
             </Layout>
-
         </Layout>
     );
 };
