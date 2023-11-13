b89dea0225d8ecc8923bf133a065d861ce9419fa
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2023-11-13 23:52:48 +0300

    Created Log Out method

--- src/components/UserArea.tsx
+++ src/components/UserArea.tsx
@@ -3,38 +3,46 @@ import { UserOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
 import { Avatar, Button, Dropdown } from 'antd';
 import { More } from 'iconsax-react';
+import { useNavigate } from 'react-router-dom';
 
-const items: MenuProps['items'] = [
-    {
-        label: <Button className='w-full font-inter'>User Update</Button>,
-        key: '0',
-    },
-    {
-        label: <Button className='w-full font-inter'>Payment</Button>,
-        key: '1',
-    },
-    {
-        type: 'divider',
-    },
-    {
-        label: <Button className='w-full font-inter'>Log Out</Button>,
-        key: '3',
-    },
-];
+const UserArea: React.FC = () => {
+    const navigate = useNavigate();
 
-const UserArea: React.FC = () => (
-    <Dropdown menu={{ items }} trigger={['click']}>
+    const logOut = () => {
+        localStorage.removeItem('token');
+        console.log("Log Out olundu");
+        navigate('/');
+    };
 
-        <a onClick={(e) => e.preventDefault()}>
+    const items: MenuProps['items'] = [
+        {
+            label: <Button className='w-full font-inter'>User Update</Button>,
+            key: '0',
+        },
+        {
+            label: <Button className='w-full font-inter'>Payment</Button>,
+            key: '1',
+        },
+        {
+            type: 'divider',
+        },
+        {
+            label: <Button className='w-full font-inter' onClick={logOut}>Log Out</Button>,
+            key: '3',
+        },
+    ];
 
-            <div className="flex items-center">
-                <Avatar className='mb-2' shape="square" size={36} icon={<UserOutlined />} />
-                <span className='ml-2 font-inter font-semibold text-base text-white'>User Name</span>
-                <More className='ml-2' />
-            </div>
-
-        </a>
-    </Dropdown>
-);
+    return (
+        <Dropdown menu={{ items }} trigger={['click']}>
+            <a onClick={(e) => e.preventDefault()}>
+                <div className="flex items-center">
+                    <Avatar className='mb-2' shape="square" size={36} icon={<UserOutlined />} />
+                    <span className='ml-2 font-inter font-semibold text-base text-white'>User Name</span>
+                    <More className='ml-2' />
+                </div>
+            </a>
+        </Dropdown>
+    );
+};
 
 export default UserArea;
